b0f842e494b4c1bdf8694aa9373a08fcaf91ae6b
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-07-14 03:15:05 -0400

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from . import crud, models, schemas, database
+
+# Create tables at the start
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_item(db=db, item=item)
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip)
+    return items
+
+@app.get("/items/{item_id}", response_model=schemas.Item)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.get_item(db, item_id=item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
